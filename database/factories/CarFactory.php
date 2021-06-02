12be318ea56b70f2b5983dd5a5cae563beee435e
krajko <nikolakrajinovic99@gmail.com>
2021-06-02 14:48:43 +0200

    zadatak 1

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Car;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Illuminate\Support\Arr;
+
+class CarFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Car::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'brand' => $this->faker->company(),
+            'model' => ucfirst($this->faker->word()),
+            'year' => $this->faker->year(),
+            'max_speed' => $this->faker->numberBetween(20, 300),
+            'is_automatic' => $this->faker->boolean(),
+            'engine' => Arr::random(['diesel', 'petrol', 'electric', 'hybrid']),
+            'number_of_doors' => $this->faker->numberBetween(2, 5)
+        ];
+    }
+}
